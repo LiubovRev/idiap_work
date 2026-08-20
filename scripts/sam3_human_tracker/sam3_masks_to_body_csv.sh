57e0f38d9ae1ae7522dfc8420fbc9e678410fc63
Liubov Revutska <68116481+LiubovRev@users.noreply.github.com>
2026-08-20 10:29:43 +0200

    Update sam3_masks_to_body_csv.sh

--- scripts/sam3_human_tracker/sam3_masks_to_body_csv.sh
+++ scripts/sam3_human_tracker/sam3_masks_to_body_csv.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-MASK_DIR="/idiap/temp/lrevutska/project/videos/MaskDir"
-OUTPUT="/idiap/temp/lrevutska/project/videos/sam3_body_detections.csv"
+MASK_DIR="/home/liubov/Documents/psifx_sam3/videos/7_INDIVIDUAL_14/full_video/MaskDir_manual_merged"
+OUTPUT="/home/liubov/Documents/psifx_sam3/videos/7_INDIVIDUAL_14/full_video/sam3_body_detections.csv"
 
 python scripts/sam3_masks_to_body_csv.py \
     --mask_dir "$MASK_DIR" \
-    --output "$OUTPUT"
+    --output "$OUTPUT" \
+    --min_area 1000 \
+    --border_margin 5
